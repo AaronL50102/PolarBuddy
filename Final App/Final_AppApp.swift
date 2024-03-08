5a611527b5c3a2246b869e1024599cc97c2c20cf
Eliza Liebo <s028378@students.lmsd.org>
2024-03-08 12:02:56 -0500

    Initial Commit

---
+++ Final App/Final_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_AppApp.swift
+//  Final App
+//
+//  Created by Eliza Liebo (student LM) on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
