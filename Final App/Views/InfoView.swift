eef203ed59d22329644337b4103481c60404204c
s017879 <s017879@students.lmsd.org>
2024-03-19 12:07:40 -0400

    my branch stuff!

--- Final App/Views/InfoView.swift
+++ Final App/Views/InfoView.swift
@@ -9,13 +9,54 @@ import SwiftUI
 
 struct InfoView: View {
     
-
     
     var body: some View {
-        Text("Info")
+            VStack{
+               
+                Text("Facts")
+                
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    Text("Enough plastic bottles are discarded over a year to go around the planet 4 times")
+                    Spacer()
+                    Text("More than 90% of our ocean plastics come from just 10 rivers")
+                }
+               
+                Spacer()
+                
+                HStack{
+                    Text("The largest dumping site of plastics is not a landfill, it is the pacific ocean!")
+                    Spacer()
+                    Text("The average person creates almost five pounds of trash per day")
+           
+                    }
+                
+                Spacer()
+                
+                HStack{
+                    Text("The largest dumping site of plastics is not a landfill, it is the pacific ocean!")
+                    Spacer()
+                    Text("The average person creates almost five pounds of trash per day")
+           
+                    }
+                
+                Spacer()
+                
+                HStack{
+                    Text("The largest dumping site of plastics is not a landfill, it is the pacific ocean!")
+                    Spacer()
+                    Text("The average person creates almost five pounds of trash per day")
+           
+                    }
+                
+            }.padding()
+  
     }
 }
 
+    
 struct InfoView_Previews: PreviewProvider {
     static var previews: some View {
         InfoView()
