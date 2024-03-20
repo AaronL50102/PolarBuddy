2da987f1eef85a6a109ea47f94122f57fb9323f0
Eliza Liebo <s028378@students.lmsd.org>
2024-03-20 12:44:46 -0400

    added views

---
+++ Final App/Views/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  Final App
+//
+//  Created by Eliza Liebo (student LM) on 3/15/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+            .environmentObject(User())
+    }
+}
