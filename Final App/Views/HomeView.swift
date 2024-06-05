7f173fc009add6c73b9faf5c9fa6eefd1a617ab7
Matthew Hartstein <matthewdhartstein@gmail.com>
2024-06-04 20:48:46 -0400

    small changes

--- Final App/Views/HomeView.swift
+++ Final App/Views/HomeView.swift
@@ -23,142 +23,149 @@ struct HomeView: View {
 
         ZStack{
             NavigationView{
-                VStack{
-                    Spacer()
-                    HStack{
+                ZStack {
+//                    Image("backgroundImageAgain")
+//                        .resizable()
+//                        .aspectRatio(contentMode: .fill)
+//                        .opacity(0.06)
+//                        .ignoresSafeArea()
+                    VStack{
                         Spacer()
-                        
-                        NavigationLink {
-                            StoreView()
-                        } label: {
-                            HStack{
-                                Text("Store")
-                                Image(systemName: "cart")
-                            }
-                            .foregroundColor(.black)
-                            .padding()
-                            .background(Color.lightBlue)
-                            .cornerRadius(20)
-                            .padding()
+                        HStack{
                             Spacer()
-                        }.padding(.leading, 20)
-                        
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        
-                        NavigationLink {
-                            InstructionsView()
-                        } label: {
-                            HStack{
-                                Image(systemName: "questionmark.circle.fill")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .scaleEffect(0.3)
-                                    .foregroundColor(.lightBlue)
-                                    .padding()
+                            
+                            NavigationLink {
+                                StoreView()
+                            } label: {
+                                HStack{
+                                    Text("Store")
+                                    Image(systemName: "cart")
+                                }
+                                .foregroundColor(.black)
+                                .padding()
+                                .background(Color.lightBlue)
+                                .cornerRadius(20)
+                                .padding()
+                                Spacer()
+                            }.padding(.leading, 20)
+                            
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            
+                            NavigationLink {
+                                InstructionsView()
+                            } label: {
+                                HStack{
+                                    Image(systemName: "questionmark.circle.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .scaleEffect(0.3)
+                                        .foregroundColor(.lightBlue)
+                                        .padding()
+                                }
+    //                            .foregroundColor(.black)
+    //                            .padding()
+    //                            .background(Color.lightBlue)
+    //                            .cornerRadius(20)
+    //                            .padding()
                             }
-//                            .foregroundColor(.black)
-//                            .padding()
-//                            .background(Color.lightBlue)
-//                            .cornerRadius(20)
-//                            .padding()
                         }
-                    }
 
-                    Spacer()
-                    Spacer()
-                    
-                    HStack{
-                        
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
                         Spacer()
                         Spacer()
                         
-                        ZStack{
-                            Image("polar")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .scaleEffect(2)
-                            if user.hasHat {
-//                                VStack{
-                                    Image("hat")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .scaleEffect(0.42)
-                                        .offset(x: 5, y: -137)
-//                                    Spacer()
-//                                }
-//                                .padding(.top, 35)
-//                                .padding(.leading, 10)
+                        HStack{
+                            
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            Spacer()
+                            
+                            ZStack{
+                                Image("polar")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .scaleEffect(2)
+                                if user.hasHat {
+    //                                VStack{
+                                        Image("hat")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .scaleEffect(0.42)
+                                            .offset(x: 5, y: -137)
+    //                                    Spacer()
+    //                                }
+    //                                .padding(.top, 35)
+    //                                .padding(.leading, 10)
+                                }
+                                if user.hasScarf {
+    //                                VStack{
+                                        Image("scarf")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .scaleEffect(0.65)
+                                            .offset(x: 5, y: -50)
+    //                                    Spacer()
+    //                                }
+    //                                .padding(.top, 155)
+    //                                .padding(.leading, 10)
+                                }
+                                if user.hasGlasses {
+    //                                VStack{
+                                        Image("glasses")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .scaleEffect(0.34)
+                                            .offset(x: 5, y: -110)
+    //                                    Spacer()
+    //                                }
+    //                                .padding(.top, 69)
+    //                                .padding(.leading, 9)
+                                }
+                                if user.hasBag {
+    //                                VStack{
+    //                                    Spacer()
+                                        Image("louisBag")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .scaleEffect(0.56)
+                                            .offset(x: 8, y: 30)
+    //                                }
+    //                                .padding(.bottom, 210)
+    //                                .padding(.leading, 50)
+                                }
                             }
-                            if user.hasScarf {
-//                                VStack{
-                                    Image("scarf")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .scaleEffect(0.65)
-                                        .offset(x: 5, y: -50)
-//                                    Spacer()
-//                                }
-//                                .padding(.top, 155)
-//                                .padding(.leading, 10)
+                            ZStack(alignment: .bottom) {
+                                Rectangle()
+                                    .frame(width: 35, height: 320)
+                                    .foregroundColor(.lightBlue)
+                                    .cornerRadius(50)
+                                Rectangle()
+                                    .frame(width: 35, height: CGFloat(320 * (CGFloat(user.points) / CGFloat(maxPoints))))
+                                    .foregroundColor(.lightMediumBlue)
+                                    .cornerRadius(50)
+                                Image("star")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .scaleEffect(0.4)
+                                    .offset(x: 0, y: -280)
                             }
-                            if user.hasGlasses {
-//                                VStack{
-                                    Image("glasses")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .scaleEffect(0.34)
-                                        .offset(x: 5, y: -110)
-//                                    Spacer()
-//                                }
-//                                .padding(.top, 69)
-//                                .padding(.leading, 9)
-                            }
-                            if user.hasBag {
-//                                VStack{
-//                                    Spacer()
-                                    Image("louisBag")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .scaleEffect(0.56)
-                                        .offset(x: 8, y: 30)
-//                                }
-//                                .padding(.bottom, 210)
-//                                .padding(.leading, 50)
-                            }
+                            .frame(height: 320)
+                            .padding()
                         }
-                        ZStack(alignment: .bottom) {
-                            Rectangle()
-                                .frame(width: 35, height: 320)
-                                .foregroundColor(.lightBlue)
-                                .cornerRadius(50)
-                            Rectangle()
-                                .frame(width: 35, height: CGFloat(320 * (CGFloat(user.points) / CGFloat(maxPoints))))
-                                .foregroundColor(.lightMediumBlue)
-                                .cornerRadius(50)
-                            Image("star")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .scaleEffect(0.4)
-                                .offset(x: 0, y: -280)
-                        }
-                        .frame(height: 320)
-                        .padding()
+                        Spacer()
+                        TextField("Name", text: $user.name)
+                            .padding()
+                            .font(Constants.largeFont)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.blueish)
+                        Spacer()
+                        Spacer()
                     }
-                    Spacer()
-                    TextField("Name", text: $user.name)
-                        .padding()
-                        .font(Constants.largeFont)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(.blueish)
-                    Spacer()
-                    Spacer()
                 }
             }
         }
